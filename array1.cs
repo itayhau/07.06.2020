6a17719c30dc986db80facb86bb2d3316e172917
dotnet290320 <62850561+dotnet290320@users.noreply.github.com>
2020-06-07 21:04:09 +0300

    Update array1.cs

--- array1.cs
+++ array1.cs
@@ -10,7 +10,7 @@ namespace today0706
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
 
             int student1 = 0;
             int student2 = 0;
